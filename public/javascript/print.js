68064be9ebb42f15f666bccc2ef7e4922842d735
teymitee1 <teymitee@gmail.com>
2019-04-04 12:50:17 +0100

    removed reset.css

--- public/javascript/print.js
+++ public/javascript/print.js
@@ -1,4 +1,5 @@
-let printBtn = document.getElementById("print-btn");
+
+let printBtn = document.getElementById("printBtn");
 printBtn.addEventListener('click', () => {
     var prtContent = document.getElementById("userInfo");
     var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
